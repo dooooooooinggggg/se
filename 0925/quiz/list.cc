edcabde523dfac20cd46f03a830d301b22955976
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-09-30 06:39:11 +0900

    add

---
+++ 0925/quiz/list.cc
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct list
+{
+  struct list *next;
+  int key;
+};
+
+struct list Head;
+int main()
+{
+  struct list *p = &Head;
+
+  for (int i = 0; i < 10; i++)
+  {
+    p->next = (struct list *)malloc(sizeof(struct list));
+    p = p->next;
+    p->key = i;
+    p->next = NULL;
+  }
+
+  for (p = Head.next; p != NULL; p = p->next)
+  {
+    printf("%d\n", p->key);
+  }
+
+  exit(0);
+}
