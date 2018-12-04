33969141dc22803e5861a348f8e9a0bb48473a17
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-12-04 15:16:18 +0900

    add 1204

---
+++ 1204/quiz/kr.cc
@@ -0,0 +1,114 @@
+#include "kr.h"
+
+bool
+validation(const LOCAL_DB local_db[], const int tbegin, const int tend)
+{
+  // Write your code here
+  return true;
+}
+
+void 
+transaction(int myid)
+{
+  LOCAL_DB local_db[NUM_ACCESS]; // local db
+  int oidList[NUM_ACCESS];
+  int tbegin = Tid;
+
+  // Set access pattern
+  for( int i = 0; i < NUM_ACCESS; i++) {
+    oidList[i] = myid * 1000 + i;
+  }
+
+ RETRY:
+  // Read Phase
+  for( int i = 0; i < NUM_ACCESS; i++) {
+    int oid = oidList[i];
+    local_db[oid].obj = Obj[oid];
+    local_db[oid].mode = WRITE; // No conflict
+  }
+
+  // Local modification
+  for( int i = 0; i < NUM_ACCESS; i++) {
+    if( local_db[i].mode == WRITE) { // Modify
+      local_db[i].obj.val += 3;
+    }
+  }
+
+  // Validation & Write phase
+  pthread_mutex_lock(&GiantLock);
+  int tend = Tid;  
+  bool valid = validation(local_db, tbegin, tend);
+  if( valid == false){
+    pthread_mutex_unlock(&GiantLock);    
+    goto RETRY;
+  }
+
+  // Write phase
+  for( int i = 0; i < NUM_ACCESS; i++){
+    if( local_db[i].mode == WRITE) {
+      Obj[i].val = local_db[i].obj.val;
+    }
+  }
+
+  // Increment transaction ID
+  Tid = Tid + 1;
+
+  // Register myself
+  XACT me;
+  me.tid = Tid;
+  int wid = 0;
+  for( int curid = 0; curid < NUM_ACCESS; curid++){
+    if( local_db[curid].mode == WRITE){
+      me.ws[wid] = curid;
+      wid++; // Increment counter for write set
+    }
+  }
+  XactHist.push_back(me);
+  
+  pthread_mutex_unlock(&GiantLock);    
+}
+
+void *
+worker(void *arg)
+{
+  int myid = *(int *)arg;
+  free(arg);
+  for( int i = 0; i < NUM_TX; i++) {
+    transaction(myid);
+  }
+  return NULL;
+}
+
+void
+init(void)
+{
+  pthread_mutex_init(&GiantLock, NULL);
+}
+
+void
+print_diff(struct timeval begin, struct timeval end)
+{
+  long diff = (end.tv_sec - begin.tv_sec) * 1000 * 1000 + (end.tv_usec - begin.tv_usec);
+  printf("%ld us\n", diff);
+}
+
+int
+main(void)
+{
+  init();
+  pthread_t thread[MAX_WORKER];
+  struct timeval begin, end;
+
+  gettimeofday(&begin, NULL);
+  for (int i = 0; i < MAX_WORKER; i++) {
+    int *id = (int *)calloc(1, sizeof(int)); if(!id)ERR; *id = i;
+    pthread_create(&thread[i], NULL, worker, id);
+  }
+  for (int i = 0; i < MAX_WORKER; i++) {
+    pthread_join(thread[i], NULL);
+  }
+  gettimeofday(&end, NULL);
+  print_diff(begin, end);
+
+  return 0;
+}
---
+++ 1204/quiz/kr.h
@@ -0,0 +1,33 @@
+// Kung-Robinson
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include <list>
+#include "debug.h"
+#include "param.h"
+using namespace std;
+typedef enum {READ, WRITE} ACCESS_MODE;
+// pthread_mutex_t Lock;
+typedef struct _OBJ {
+  pthread_mutex_t lock;  
+  int key;
+  int val;
+} OBJ;
+OBJ Obj[MAX_OBJ];
+
+typedef struct _LOCAL_DB {
+  ACCESS_MODE mode; // read or write
+  OBJ obj;
+} LOCAL_DB;
+
+typedef struct _XACT {
+  int tid;
+  int nwrite;
+  int ws[MAX_OBJ];
+} XACT;
+
+list<XACT> XactHist;
+int Tid = 0;
+pthread_mutex_t GiantLock;
