22bb8f181a2a9907c25965c0025dc8078349892e
dooooooooinggggg <dooooooooinggggg@gmail.com>
2019-01-25 02:21:18 +0900

    update

--- 0122/quiz/main.cc
+++ 0122/quiz/main.cc
@@ -5,20 +5,21 @@
 
 OBJ TmpObj[MAX_OBJ]; // tmp obj for redo and undo
 
-void
-dispTxTable(void)
+void dispTxTable(void)
 {
-  for( auto itr = TxTable.begin(); itr != TxTable.end(); itr++ ){
+  for (auto itr = TxTable.begin(); itr != TxTable.end(); itr++)
+  {
     printf("%4d:%4d:%d\n", itr->txid, itr->llsn, itr->stat);
   }
 }
 
-void
-updateTxTable(int txid, int lsn, TX_STAT stat)
+void updateTxTable(int txid, int lsn, TX_STAT stat)
 {
-  for (auto itr=TxTable.begin(); itr!=TxTable.end(); itr++) {
-    if (itr->txid == txid) {
-      itr->llsn = lsn; 
+  for (auto itr = TxTable.begin(); itr != TxTable.end(); itr++)
+  {
+    if (itr->txid == txid)
+    {
+      itr->llsn = lsn;
       return;
     }
   }
@@ -30,11 +31,12 @@ updateTxTable(int txid, int lsn, TX_STAT stat)
   TxTable.push_back(tt);
 }
 
-void
-finalizeTxTable(int txid, TX_STAT stat)
+void finalizeTxTable(int txid, TX_STAT stat)
 {
-  for( auto itr=TxTable.begin(); itr!=TxTable.end(); itr++ ){
-    if (itr->txid == txid) {
+  for (auto itr = TxTable.begin(); itr != TxTable.end(); itr++)
+  {
+    if (itr->txid == txid)
+    {
       itr->stat = stat;
       return;
     }
@@ -42,86 +44,108 @@ finalizeTxTable(int txid, TX_STAT stat)
   ERR;
 }
 
-void
-redo_test()
+void redo_test()
 {
   LOG l;
-  for (int i = 0; i < MAX_OBJ; i++) {
+  for (int i = 0; i < MAX_OBJ; i++)
+  {
     TmpObj[i].lsn = -1;
   }
 
   // Redo
-  int fd = open(LOG_FILE, O_RDONLY); if( fd == -1 )ERR;
-  while (true){
+  int fd = open(LOG_FILE, O_RDONLY);
+  if (fd == -1)
+    ERR;
+  while (true)
+  {
     int ret = read(fd, &l, sizeof(LOG));
-    if (ret == 0) break; else if (ret<0) ERR;
+    if (ret == 0)
+      break;
+    else if (ret < 0)
+      ERR;
 
     // Redo core
-    if (l.type == UPDATE) {
+    if (l.type == UPDATE)
+    {
       TmpObj[l.oid].val = l.after;
       TmpObj[l.oid].lsn = l.lsn;
       updateTxTable(l.txid, l.lsn, INPROGRESS);
     }
-    else if (l.type == COMMIT) {
+    else if (l.type == COMMIT)
+    {
       finalizeTxTable(l.txid, COMMITING);
     }
-    else if (l.type == ABORT) {
+    else if (l.type == ABORT)
+    {
       finalizeTxTable(l.txid, ABORTING);
     }
   }
-  int ret = close(fd); if( ret<0 )ERR;
+  int ret = close(fd);
+  if (ret < 0)
+    ERR;
 
   // State check for REDO
-  for (int i = 0; i < MAX_OBJ; i++) {
-    if (Obj[i].lsn != -1) {
-      if (Obj[i].lsn == TmpObj[i].lsn && Obj[i].val == TmpObj[i].val) /* ok */;
-      else ERR;
+  for (int i = 0; i < MAX_OBJ; i++)
+  {
+    if (Obj[i].lsn != -1)
+    {
+      if (Obj[i].lsn == TmpObj[i].lsn && Obj[i].val == TmpObj[i].val) /* ok */
+        ;
+      else
+        ERR;
     }
   }
 }
 
-void
-safe_terminate(void)
+void safe_terminate(void)
 {
   int ret = close(Logfd);
-  if( ret<0 ) ERR;
+  if (ret < 0)
+    ERR;
 }
 
-void
-undo_core(int lsn)
+void undo_core(int lsn)
 {
   int fd;
   int n;
   LOG l;
 
-  fd = open(LOG_FILE, O_RDONLY); if( fd == -1 )ERR;
-  while (true) {
+  fd = open(LOG_FILE, O_RDONLY);
+  if (fd == -1)
+    ERR;
+  while (true)
+  {
     n = lseek(fd, lsn * sizeof(LOG), SEEK_SET);
-    n = read(fd, &l, sizeof(LOG)); if( n<0 )ERR;
+    n = read(fd, &l, sizeof(LOG));
+    if (n < 0)
+      ERR;
     TmpObj[l.oid].val = l.before;
-    if (l.plsn == -1) break;
+    if (l.plsn == -1)
+      break;
     lsn = l.plsn;
   }
-  n = close(fd); if (n<0) ERR;
+  n = close(fd);
+  if (n < 0)
+    ERR;
   printf("Undo Success\n");
 }
 
-void
-undo_test()
+void undo_test()
 {
   dispTxTable();
-  for (auto itr=TxTable.begin(); itr!=TxTable.end(); itr++){
-    if (itr->stat != COMMITING) {
+  for (auto itr = TxTable.begin(); itr != TxTable.end(); itr++)
+  {
+    if (itr->stat != COMMITING)
+    {
       undo_core(itr->llsn);
     }
   }
 }
 
-void 
-sig_handler(int sig)
+void sig_handler(int sig)
 {
-  printf("Bye bye---, your database is inconsistent ^_^/ \n");
-  exit(0);
+  // printf("Bye bye---, your database is inconsistent ^_^/ \n");
+  // exit(0);
 
   printf("Received Signal\n");
   safe_terminate();
@@ -134,25 +158,26 @@ sig_handler(int sig)
   exit(0);
 }
 
-void
-init(void)
+void init(void)
 {
   // Signal
-  if (signal(SIGINT, sig_handler) == SIG_ERR) ERR;
+  if (signal(SIGINT, sig_handler) == SIG_ERR)
+    ERR;
 
   // File
-  Logfd = open(LOG_FILE, O_WRONLY|O_CREAT|O_APPEND|O_TRUNC, 0644);
-  if (Logfd == -1) ERR;
+  Logfd = open(LOG_FILE, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0644);
+  if (Logfd == -1)
+    ERR;
 
   // Init database
-  for (int i = 0; i < MAX_OBJ; i++) {
+  for (int i = 0; i < MAX_OBJ; i++)
+  {
     Obj[i].lsn = -1;
     Obj[i].val = i * 10;
   }
 }
 
-int
-getLsn(void)
+int getLsn(void)
 {
   // lock
   static int lsn = 0;
@@ -161,8 +186,7 @@ getLsn(void)
   return lsn;
 }
 
-LOG
-getCommitLog(int txid)
+LOG getCommitLog(int txid)
 {
   LOG l;
   l.txid = txid;
@@ -171,8 +195,7 @@ getCommitLog(int txid)
   return l;
 }
 
-LOG
-getUpdateLog(int txid, int oid, int after)
+LOG getUpdateLog(int txid, int oid, int after)
 {
   LOG l;
 
@@ -187,13 +210,13 @@ getUpdateLog(int txid, int oid, int after)
   return l;
 }
 
-void
-normal(const int txid)
+void normal(const int txid)
 {
   LOG l;
   int nupdate = rand() % 100 + 1;
 
-  for (int i = 0; i < nupdate; i++) {
+  for (int i = 0; i < nupdate; i++)
+  {
     usleep(1000);
 
     // Update an object
@@ -202,7 +225,9 @@ normal(const int txid)
 
     // Make a log
     l = getUpdateLog(txid, oid, after);
-    int ret = write(Logfd, &l, sizeof(l)); if(ret<0)ERR;
+    int ret = write(Logfd, &l, sizeof(l));
+    if (ret < 0)
+      ERR;
 
     // Update database
     Obj[oid].val = after;
@@ -212,17 +237,19 @@ normal(const int txid)
     updateTxTable(l.txid, l.lsn, INPROGRESS);
   }
   l = getCommitLog(txid);
-  int ret = write(Logfd, &l, sizeof(l)); if(ret<0)ERR;
+  int ret = write(Logfd, &l, sizeof(l));
+  if (ret < 0)
+    ERR;
   finalizeTxTable(l.txid, COMMITING);
 }
 
-int
-main(void)
+int main(void)
 {
   init();
   std::cout << "Init Done" << std::endl;
 
-  for( int txid = 1; txid <= MAX_TRANXACT; txid++ ){
+  for (int txid = 1; txid <= MAX_TRANXACT; txid++)
+  {
     printf("Trans: %d\n", txid);
     normal(txid);
   }
